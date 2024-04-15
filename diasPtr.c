a94cb417f38d418b0ca26dd9b255aff10136a6ef
gabriel_delece <103008339+caff3in3@users.noreply.github.com>
2024-04-15 11:58:14 -0600

    15-04

---
+++ diasPtr.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  dias
+//
+//  Created by CEDAM 12 on 15/04/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    char *dias[] = {"lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"};
+    
+    printf("El dia es: %s\n", dias[3]);
+    
+    return 0;
+}
